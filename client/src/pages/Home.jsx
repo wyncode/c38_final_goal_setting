4f6f9287696b67f183cb3b812c72b78bfe668a46
Frances Dalla Torre <francesdallatorre@hotmail.com>
2020-08-24 19:18:01 -0400

    made homepage and footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import HomePage from '../components/HomePage';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Navigation />
+      <HomePage />
+      <Footer />
     </div>
   );
 };
