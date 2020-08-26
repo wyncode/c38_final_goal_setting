7c3f8656acded6aff648a90eadbc209f008a1209
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2020-08-25 23:01:42 -0400

    fixed chapter utility function

--- client/src/pages/Chapter.jsx
+++ client/src/pages/Chapter.jsx
@@ -1,29 +1,27 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { Container, ProgressBar, Button, Card } from 'react-bootstrap';
 
-const Chapter = () => {
+const Chapter = ({ history }) => {
   const { currentChapter, currentStory } = useContext(AppContext);
-  const history = useHistory();
-  const progress =
-    ((currentChapter?.currentIndex + 1) * 100) / currentStory?.chapters.length;
-  console.log(progress);
-
   if (!currentChapter) history.push('/dashboard');
-  console.log(currentStory);
+
+  const progress =
+    2 + (currentChapter?.index * 100) / currentStory?.chapters.length;
+
+  console.log(currentChapter);
 
   return (
     <Container className="d-flex flex-column">
       <h1 style={{ textAlign: 'center' }}>Goal Book</h1>
       <div>
-        <h2>Chapter: {currentChapter?.currentIndex + 1}</h2>
+        <h2>Chapter: {currentChapter?.index + 1}</h2>
         <h2>Total Chapters: {currentStory?.chapters.length}</h2>
       </div>
       <h2>Goal progress board</h2>
       <ProgressBar now={progress} />
       <h2>Daily Tasks</h2>
-      {currentChapter?.description}
+      <h3>{currentChapter?.data.description}</h3>
       <h2>Reflections</h2>
       <Button>Add a reflection</Button>
     </Container>
